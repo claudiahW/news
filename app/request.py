dc850ee104672aa9b2495fbd57ccef61d836b915
claudiahW <murungiclaud@gmail.com>
2021-11-01 15:31:07 +0300

    create for loop for the latest news section

--- app/request.py
+++ app/request.py
@@ -9,3 +9,45 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        source = news_item.get('source')
+        image = news_item.get('image_path')
+        description = news_item.get('description')
+        time = news_item.get('time')
+
+        if image:
+            news_object = News(id,title,source,image,description,time)
+            news_results.append(news_object)    
+
+
+    return news_results
--- app/views.py
+++ app/views.py
@@ -1,18 +1,21 @@
 from flask import render_template
 from app import app
-
+from .request import get_news 
 # Views
 @app.route('/')
 def index():
     '''
     View news page function that returns the index page and its data
     '''
+    #getting latest news
+    latest_news =get_news('latest')
+    print(latest_news)
 
     title = 'Home - Welcome to The best news site Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title , latest =latest_news)
 
 @app.route('/news/<news_id>')
-def movie(news_id):
+def news(news_id):
 
     '''
     View news page function that returns the news details page and its data
