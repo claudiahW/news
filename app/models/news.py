10d0041abb7b92be57ff6583ac91d9d8ff6bf08d
claudiahW <murungiclaud@gmail.com>
2021-11-01 15:00:16 +0300

    first api call

---
+++ app/models/news.py
@@ -0,0 +1,13 @@
+class News:
+    '''
+    News class that will define news objects
+    ''' 
+def __init__(self,id,title,source,image,description,time):
+        self.id =id
+        self.title = title
+        self.source = source
+        self.image =image 
+        self.description =description
+        self.time =time 
+       
+        
